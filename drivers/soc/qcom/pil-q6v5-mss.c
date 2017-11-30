46aeac192c698768daac84dd035506ebd69f97c4
Jitendra Sharma <shajit@codeaurora.org>
2017-11-30 16:50:35 +0530

    soc: qcom: pil: Remove null character writing of shared mem
    PIL is writing '\0' in the shared mem region. This
    region is shared with other clients that are also trying
    to read. Hence they miss first character.
    
    Remove this null character writing by PIL, as this mem
    area is supposed to be Read only.
    
    Change-Id: Ib7671256ca301afae4ab69bb1f8a87c529dc10cc
    Signed-off-by: Jitendra Sharma <shajit@codeaurora.org>
    Signed-off-by: celtare21 <celtare21@gmail.com>
    Signed-off-by: nysascape <nysadev@raphielgang.org>

--- drivers/soc/qcom/pil-q6v5-mss.c
+++ drivers/soc/qcom/pil-q6v5-mss.c
@@ -63,8 +63,7 @@ static void log_modem_sfr(void)
 	strlcpy(reason, smem_reason, min(size, MAX_SSR_REASON_LEN));
 	pr_err("modem subsystem failure reason: %s.\n", reason);
 
-	smem_reason[0] = '\0';
-	wmb();
+	strlcpy(mfr_failure_reason, smem_reason, min(size, MAX_SSR_REASON_LEN));
 }
 
 static void restart_modem(struct modem_data *drv)
